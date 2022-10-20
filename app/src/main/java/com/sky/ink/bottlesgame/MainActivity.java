78c3d04c77098bebc927237894d1c5f5827fd5d2
dalton <daltonchivambo@gmail.com>
2022-10-20 08:26:18 +0200

    Implementing events.

--- app/src/main/java/com/sky/ink/bottlesgame/MainActivity.java
+++ app/src/main/java/com/sky/ink/bottlesgame/MainActivity.java
@@ -3,6 +3,9 @@ package com.sky.ink.bottlesgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import java.util.Random;
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iv_bottle = findViewById(R.id.iv_bottle);
+
+        iv_bottle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int new_direction = random.nextInt(1800);
+                // creating axis
+                float axis_x = iv_bottle.getWidth() / 2;
+                float axis_y = iv_bottle.getHeight() / 2;
+
+                //Creating animation (1800 for 360 is based in 5 spin)
+                Animation spin = new RotateAnimation(last_direction,new_direction,axis_x,axis_y);
+                spin.setDuration(3000);
+                // for non return to original position
+                spin.setFillAfter(true);
+
+                last_direction = new_direction;
+                iv_bottle.startAnimation(spin);
+            }
+        });
     }
 }
