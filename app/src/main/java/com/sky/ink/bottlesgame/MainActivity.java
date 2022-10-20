e2f26a053ff1c37df8480d2a9cf7f1718dca3cdc
dalton <daltonchivambo@gmail.com>
2022-10-20 08:13:45 +0200

    Implementing the components on MainActivity

--- app/src/main/java/com/sky/ink/bottlesgame/MainActivity.java
+++ app/src/main/java/com/sky/ink/bottlesgame/MainActivity.java
@@ -3,12 +3,22 @@ package com.sky.ink.bottlesgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_bottle;
+    private  Random random = new Random();
+    private  int last_direction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_bottle = findViewById(R.id.iv_bottle);
     }
 }
